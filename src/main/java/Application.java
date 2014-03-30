6e40a7fc7864c4017d28bdcb1125c243a4ea645f
Justin Woo <moomoowoo@gmail.com>
2014-03-30 00:25:53 -0500

    removed "new" behavior due to stupidity and race conditioning

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -4,7 +4,6 @@ import java.awt.Robot;
 import java.awt.event.KeyEvent;
 import java.io.BufferedReader;
 import java.io.FileReader;
-import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -21,6 +20,9 @@ public class Application {
 //        Time to alt-tab into Skullgirls
         Thread.sleep(1000);
 
+//        Make Big Band pick up his trumpet to JAM!!!
+        initTrumpet(robot);
+
 //        Play the notes in specified file or notes.csv
         String file = "notes.csv";
         if (args.length == 1) {
@@ -29,103 +31,76 @@ public class Application {
         FileReader fr = new FileReader(file);
         BufferedReader br = new BufferedReader(fr);
         String line;
-
-//        initialize keypress buffers
-        List<Integer> previousKeys1P = null;
-        List<Integer> previousKeys2P = null;
-        List<Integer> nextKeys1P = null;
-        List<Integer> nextKeys2P = null;
+        List<Integer> keys = null;
         String rest;
 
-//        Make Big Band pick up his trumpet to JAM!!!
-        initTrumpet(robot, nextKeys1P, previousKeys1P, nextKeys2P, previousKeys2P);
-
         while ((line = br.readLine()) != null) {
+//            release the previous note's keys
+            if (keys != null) {
+                releaseKeys(robot, keys);
+            }
+
 //            C1,200 gets split into C1 and 200
             String split[] = line.split(",");
             if (split.length == 2) { // If line only contains one note
                 String note = split[0];
                 rest = split[1];
 //            Transcribe the note into the keystrokes needed
-                nextKeys1P = Transcriber.transcribe(note);
-                if (!nextKeys1P.isEmpty() && previousKeys1P != null) {
-                    releaseKeys(robot, previousKeys1P);
-                }
-                pressKeys(robot, nextKeys1P);
-                previousKeys1P = nextKeys1P;
+                keys = Transcriber.transcribe(note);
             } else if (split.length == 3) { // If line only contains two notes
                 String note1 = split[0];
                 String note2 = split[1];
                 rest = split[2];
-                nextKeys1P = Transcriber.transcribe(note1);
-                nextKeys2P = Transcriber.translateTo2P(Transcriber.transcribe(note2));
-
-                if (!nextKeys1P.isEmpty() && previousKeys1P != null) {
-                    releaseKeys(robot, previousKeys1P);
-                }
-                if (!nextKeys2P.isEmpty() && previousKeys2P != null) {
-                    releaseKeys(robot, previousKeys2P);
-                }
-
-                pressKeys(robot, nextKeys1P);
-                pressKeys(robot, nextKeys2P);
-                previousKeys1P = nextKeys1P;
-                previousKeys2P = nextKeys2P;
+                keys = Transcriber.transcribe(note1);
+                List<Integer> keys2 = Transcriber.transcribe(note2);
+                keys.addAll(Transcriber.translateTo2P(keys2));
             } else {
                 throw new IllegalArgumentException("Line contains more than three elements.");
             }
 
+
+//            Play the note and then release the keys
+            for (Integer key : keys) {
+                if (key != null) {
+                    robot.keyPress(key);
+                }
+            }
 //            Rest after the note has been played.
             Thread.sleep(Long.parseLong(rest));
         }
 
 //        Release keys one last time
-        if (previousKeys1P != null) {
-            releaseKeys(robot, previousKeys1P);
-        }
-        if (previousKeys2P != null) {
-            releaseKeys(robot, previousKeys2P);
-        }
+        releaseKeys(robot, keys);
 
         System.out.println("All done");
-    }
-
-    private static void initTrumpet(Robot robot, List<Integer> nextKeys1P, List<Integer> previousKeys1P, List<Integer> nextKeys2P, List<Integer> previousKeys2P) throws InterruptedException {
-//        pick up the trumpet
-        robot.keyPress(KeyEvent.VK_S);
-        robot.keyPress(KeyEvent.VK_I);
-        Thread.sleep(100);
-        robot.keyRelease(KeyEvent.VK_S);
-        robot.keyRelease(KeyEvent.VK_I);
-
-//        play a blank note and then queue up the release
-        nextKeys1P = new LinkedList<>();
-        nextKeys2P = new LinkedList<>();
-        nextKeys1P.add(KeyEvent.VK_Z);
-        nextKeys1P.add(KeyEvent.VK_X);
-        nextKeys1P.add(KeyEvent.VK_C);
-        nextKeys2P.add(KeyEvent.VK_J);
-        nextKeys2P.add(KeyEvent.VK_K);
-        nextKeys2P.add(KeyEvent.VK_L);
-        pressKeys(robot, nextKeys1P);
-        pressKeys(robot, nextKeys2P);
-        previousKeys1P = nextKeys1P;
-        previousKeys2P = nextKeys2P;
-
-        Thread.sleep(1000);
-    }
-
-    private static void pressKeys(Robot robot, List<Integer> keys) {
-        for (Integer key : keys) {
-            if (key != null) {
-                robot.keyPress(key);
-            }
-        }
     }
 
     private static void releaseKeys(Robot robot, List<Integer> keys) {
         for (Integer key: keys) {
             robot.keyRelease(key);
         }
+    }
+
+    private static void initTrumpet(Robot robot) throws InterruptedException {
+        robot.keyPress(KeyEvent.VK_S);
+        robot.keyPress(KeyEvent.VK_I);
+        Thread.sleep(200);
+        robot.keyRelease(KeyEvent.VK_S);
+        robot.keyRelease(KeyEvent.VK_I);
+
+        robot.keyPress(KeyEvent.VK_Z);
+        robot.keyPress(KeyEvent.VK_X);
+        robot.keyPress(KeyEvent.VK_C);
+        robot.keyPress(KeyEvent.VK_J);
+        robot.keyPress(KeyEvent.VK_K);
+        robot.keyPress(KeyEvent.VK_L);
+        Thread.sleep(40);
+        robot.keyRelease(KeyEvent.VK_Z);
+        robot.keyRelease(KeyEvent.VK_X);
+        robot.keyRelease(KeyEvent.VK_C);
+        robot.keyRelease(KeyEvent.VK_J);
+        robot.keyRelease(KeyEvent.VK_K);
+        robot.keyRelease(KeyEvent.VK_L);
+        Thread.sleep(200);
     }
 }
